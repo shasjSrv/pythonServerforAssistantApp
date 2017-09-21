a0a40b82c03724290bc46df86252e2a913d6f814
jzy <shasj@163.com>
2017-09-21 10:55:20 +0800

    add updateMedicineInfo interface

--- DBInterface.py
+++ DBInterface.py
@@ -1,5 +1,7 @@
 """This module implements an interface of mysql DB."""
+from datetime import date
 import MySQLdb
+
 
 
 SETTINGS = {}
@@ -8,8 +10,14 @@ SETTINGS['MYSQL_HOST'] = 'localhost'
 SETTINGS['MYSQL_DBNAME'] = 'MedicalUserInfo'
 SETTINGS['MYSQL_USER'] = 'SCUT'
 SETTINGS['MYSQL_PASSWD'] = 'SCUT1234'
+
+
 PATIENT = 0
 NURSE = 1
+###
+# the limit days of medicine that would be sent
+###
+DURATION = 4
 
 
 class DB(object):
@@ -42,13 +50,7 @@ class DB(object):
         cursor.close()
         self.conn.close()
         return onedata
-        # except (AttributeError, MySQLdb.OperationalError):
-        #     self.connect()
-        #     cursor = self.conn.cursor()
-        #     cursor.execute("SELECT rfid FROM userInfo WHERE userID = \
-        #     (SELECT userID FROM roomUserInfo WHERE room_no = %s and berth_no = %s)"
-        #                    , (room_no, berth_no))
-        # return cursor
+       
     
     def queryUserName(self, user_id):
         # try:
@@ -58,61 +60,29 @@ class DB(object):
         cursor.close()
         self.conn.close()
         return onedata
-        # except (AttributeError, MySQLdb.OperationalError):
-        #     self.connect()
-        #     cursor = self.conn.cursor()
-        #     cursor.execute("select userName,type from userInfo where userID = %s", (user_id,))
-        # return cursor
+        
 
     def queryUserMedicineInfo(self, user_id):
         # try:
         cursor = self.conn.cursor()
         cursor.execute("SELECT mui.userID,mi.medicineName,mi.medicineDosage,mi.unit,\
-        mui.number,mui.isSend from medicalUserInfo as mui left join medicineInfo as mi on \
+        mui.number,mui.isSend,mi.medicineID from medicalUserInfo as mui left join medicineInfo as mi on \
         mi.medicineID = mui.medicineID  left join userInfo as u on\
         mui.userID = u.userID where mui.userID = %s", (user_id,))
         alldata = cursor.fetchall()
         cursor.close()
         self.conn.close()
         return alldata
-        # except (AttributeError, MySQLdb.OperationalError):
-        #     self.connect()
-        #     cursor = self.conn.cursor()
-        #     cursor.execute("SELECT mui.userID,mi.medicineName,mi.medicineDosage,mi.unit,\
-        #     mui.number,mui.isSend from medicalUserInfo as mui left join medicineInfo as mi on \
-        #     mi.medicineID = mui.medicineID  left join userInfo as u on\
-        #     mui.userID = u.userID where mui.userID = %s", (user_id,))
-        # return cursor
-    
-    def updateMedicineInfo(self, user_id, medicine_id):
-        # try:
-        cursor = self.conn.cursor()
-        cursor.execute("update medicalUserInfo set isSend = 1 where \
-        userID = %s and medicineID = %s", (user_id, medicine_id))
-        alldata = cursor.fetchall()
-        cursor.close()
-        self.conn.close()
-        return alldata
-        # except (AttributeError, MySQLdb.OperationalError):
-        #     self.connect()
-        #     cursor = self.conn.cursor()
-        #     cursor.execute("update medicalUserInfo set isSend = 1 where \
-        #     userID = %s and medicineID = %s", (user_id, medicine_id))
-        # return cursor
-
+        
     def queryPatient(self):
         # try:
         cursor = self.conn.cursor()
-        cursor.execute("select userName,userID from userInfo where type = %s", (PATIENT,))
+        cursor.execute("select userName,userID,rfid from userInfo where type = %s", (PATIENT,))
         alldata = cursor.fetchall()
         cursor.close()
         self.conn.close()
         return alldata
-        # except (AttributeError, MySQLdb.OperationalError):
-        #     self.connect()
-        #     cursor = self.conn.cursor()
-        #     cursor.execute("select userName,userID from userInfo where type = %s", (PATIENT,))
-        # return cursor
+        #
 
     def queryRoomRfid(self, room_no):
         # try:
@@ -122,9 +92,16 @@ class DB(object):
         cursor.close()
         self.conn.close()
         return onedata
-        # except (AttributeError, MySQLdb.OperationalError):
-        #     self.connect()
-        #     cursor = self.conn.cursor()
-        #     cursor.execute("select rfid from roomInfo where room_no = %s", (room_no,))
-        # return cursor
-
+        
+    def updateMedicineInfo(self, user_id, medicine_id, date_yyyy, date_mm, date_dd):
+        # try:
+        cursor = self.conn.cursor()
+        cursor.execute("update medicalUserInfo set isSend = 1 where \
+        userID = %s and medicineID = %s and dateTime BETWEEN %s AND %s "\
+        , (user_id, medicine_id, date(int(date_yyyy), int(date_mm), int(date_dd) - DURATION)\
+        , date(int(date_yyyy), int(date_mm), int(date_dd))))
+        
+        self.conn.commit()
+        cursor.close()
+        self.conn.close()
+        return True
--- mysqlServer.py
+++ mysqlServer.py
@@ -1,5 +1,7 @@
 """This module is interface to mysql."""
+import sys
 from DBInterface import DB
+
 
 from flask import Flask, jsonify
 from flask import request
@@ -67,15 +69,14 @@ def query_id_info():
         'type' : -1,
         'patientNameArray': [],
         'patientIDArray':[],
+        'patientRfIDArray':[],
         'medicineNameArray':[],
         'medicineCountArray':[],
-        'medicineDosageArray':[]
+        'medicineDosageArray':[],
+        'medicineIDArray':[]
     }
 
     user_id = request.json['user_id']
-    
-    # cursor = MYDB.queryUserName(user_id)
-    # ret = cursor.fetchone()
     query_user_name_db = DB()
     ret = query_user_name_db.queryUserName(user_id)
     if ret is None:
@@ -87,10 +88,10 @@ def query_id_info():
     respose['isSuccess'] = 1
     respose['userName'] = user_name
     respose['type'] = user_type
-    # cursor = MYDB.queryPatient()
-    # ret = cursor.fetchall()
     query_patient_db = DB()
     ret = query_patient_db.queryPatient()
+    #3 columns: 0   patientName, 1  patientID, 2    patientRfID
+
     if ret is None:
         return jsonify({'result' : respose}), 202
     
@@ -100,37 +101,69 @@ def query_id_info():
         medicine_name_list = []
         medicine_measure_list = []
         medicine_dosage_list = []
+        medicine_id_list = []
         # cursor = MYDB.queryUserMedicineInfo(ret[i][1])
         # ret_medicine = cursor.fetchall()
         query_medecine_info_db = DB()
         ret_medicine = query_medecine_info_db.queryUserMedicineInfo(ret[i][1])
         
         for j in range(0, len(ret_medicine)):
-            #6 columns: 0 userID,1 medicineName,2 medicineDosage
-            #           3 uNit, 4 number, 5 isSend
+            #7 columns: 0 userID,1 medicineName,2 medicineDosage
+            #           3 uNit, 4 number, 5 isSend, 6 medicineID
             is_send = ret_medicine[j][5]
             #if the medicine is sent,jump to next one
             if is_send == 1:    
                 continue
-            # medicine_send_info_list.append(is_send)
             measure = str(ret_medicine[j][4]) + ret_medicine[j][3]
             medicine_name_list.append(ret_medicine[j][1])
             medicine_measure_list.append(measure)
             medicine_dosage_list.append(ret_medicine[j][2])
+            medicine_id_list.append(ret_medicine[j][6])
 
         print len(medicine_name_list)
         if len(medicine_name_list) != 0:
             respose['patientNameArray'].append(ret[i][0])
             respose['patientIDArray'].append(ret[i][1])
+            respose['patientRfIDArray'].append(ret[i][2])
             respose['medicineNameArray'].append(medicine_name_list)
             respose['medicineCountArray'].append(medicine_measure_list)
             respose['medicineDosageArray'].append(medicine_dosage_list)
-        
-
-
-
+            respose['medicineIDArray'].append(medicine_id_list)
     return jsonify({'result' : respose}), 201
 
+
+@APP.route('/UpdateUIDMID', methods=['POST'])
+def update_medicine_id_info():
+    """
+    This function is about Query user describ and user type.
+    """
+    if not request.json or not 'user_id' \
+    or not 'medicine_id_arraylist' \
+    or not 'date_yyyy'\
+    or not 'date_mm'\
+    or not 'date_dd' in request.json:
+        abort(400)
+    respose = {
+        'updateSuccess' : 0
+    }
+
+    user_id = request.json['user_id']
+    medicine_id_arraylist = list(request.json['medicine_id_arraylist'])
+    date_yyyy = request.json['date_yyyy']
+    date_mm = request.json['date_mm']
+    date_dd = request.json['date_dd']
+
+    print user_id
+    respose['updateSuccess'] = 1
+    print medicine_id_arraylist[0]
+    for i in range(0, len(medicine_id_arraylist)):
+        update_medicine_db = DB()
+        update_success = update_medicine_db.updateMedicineInfo(user_id, medicine_id_arraylist[i] \
+        , date_yyyy, date_mm, date_dd)
+        if update_success is False:
+            respose['updateSuccess'] = 0
+
+    return jsonify({'result' : respose}), 201
 
 # @app.route('/sendIDStatuse', methods=['POST'])
 # def create_task():
@@ -164,5 +197,6 @@ def not_found(error):
     return make_response(jsonify({'error': 'Not found'}), 404)
 
 if __name__ == '__main__':
-    
+    reload(sys) 
+    sys.setdefaultencoding('utf-8')
     APP.run(host='0.0.0.0',port = 5000)
